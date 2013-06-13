e2ea1bc4aff2a2ad28c4dfb433a9f2feff6b4ec0
zendawg <rich@statefive.org>
2013-06-13 15:26:14 +0100

    Fixed code error.

--- migrations/m130612_193837_study_date_time_column_type.php
+++ migrations/m130612_193837_study_date_time_column_type.php
@@ -4,13 +4,13 @@ class m130612_193837_study_date_time_column_type extends CDbMigration {
 
   // Use safeUp/safeDown to do migration with transaction
   public function safeUp() {
-    alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_date', 'date');
-    alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_time', 'time');
+    $this->alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_date', 'date');
+    $this->alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_time', 'time');
   }
 
   public function safeDown() {
-    alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_date', 'varchar(10)');
-    alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_time', 'varchar(12)');
+    $this->alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_date', 'varchar(10)');
+    $this->alterColumn('ophscimagehumphreys_scan_humphrey_xml', 'study_time', 'varchar(12)');
   }
 
 }
